6e2980dc443ffdf8f2ce4f5414744acf3f75af6e
Shukura Bakare <bshukuralillahi@cauridor.com>
2024-06-30 23:53:20 +0000

    swiper card content

--- src/components/modules/home/slider/index.tsx
+++ src/components/modules/home/slider/index.tsx
@@ -1,18 +1,17 @@
 "use client";
 import React, { useRef, useState, useEffect } from 'react';
 import './style.scss';
+import Image from "next/image";
+import ghana from "../../../assets/ghana.png";
 
 const cards = [
-  { id: 1, content: 'Card 1' },
-  { id: 2, content: 'Card 2' },
-  { id: 3, content: 'Card 3' },
-  { id: 4, content: 'Card 4' },
-  { id: 5, content: 'Card 3' },
-  { id: 6, content: 'Card 4' },
-  { id: 7, content: 'Card 3' },
-  { id: 8, content: 'Card 4' },
-  { id: 9, content: 'Card 3' },
-  { id: 10, content: 'Card 4' },
+  { id: 1, content: 'I frequently use the BnB CashApp, and I must say that I am very satisfied. In no time, the person on the other end receives their money. I highly recommend it, it is very convenient.', name:'M’balou Cissé',image:ghana,country:'guinea'},
+  { id: 2, content: 'My experience with the BnB Cash app has been fantastic. Ive been using the app for two years to send money to my brother in Rwanda, and he receives it directly into his mobile wallet. Additionally, I use it to receive money from abroad through Ria, which really saves me a lot of time. ', name:'M’balou Cissé',image:ghana,country:'guinea' },
+  { id: 3, content: 'One of the best things that happened to Liberia was BnB overseas money transfers. With their APP you no longer need to go to the bank again to receive money from Moneygram, Ria or WesternUnion.', name:'M’balou Cissé' ,image:ghana,country:'guinea'},
+  { id: 4, content: 'One of the best things that happened to Liberia was BnB overseas money transfers. With their APP you no longer need to go to the bank again to receive money from Moneygram, Ria or WesternUnion. ', name:'M’balou Cissé',image:ghana,country:'guinea' },
+  { id: 5, content: 'One of the best things that happened to Liberia was BnB overseas money transfers. With their APP you no longer need to go to the bank again to receive money from Moneygram, Ria or WesternUnion', name:'M’balou Cissé' ,image:ghana,country:'guinea'},
+  { id: 6, content: 'I frequently use the BnB CashApp, and I must say that I am very satisfied. In no time, the person on the other end receives their money. I highly recommend it, it is very convenient.', name:'M’balou Cissé',image:ghana,country:'guinea' },
+  
   // Add more cards if needed
 ];
 

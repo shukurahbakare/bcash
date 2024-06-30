cc61eafade96ca30c363d02c1c1899e2e8a62425
drdonice <doniceubaru1@gmail.com>
2024-06-30 23:12:17 +0100

    Fixed

--- src/components/modules/home/features/index.tsx
+++ src/components/modules/home/features/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { PiRocketLaunchDuotone, PiShieldCheckDuotone, PiUserFocusDuotone } from "react-icons/pi";
+import Image from "next/image";
+import "./style.scss";
+import rocket from "@/components/assets/icons/IconRocket.svg";
+import support from "@/components/assets/icons/IconSupport.svg";
+import sheild from "@/components/assets/icons/IconSheild.svg";
 
 const features: {
   name: string;
@@ -7,22 +11,22 @@ const features: {
   icon: any;
 }[] = [
   {
-    icon: <PiUserFocusDuotone/>,
+    icon: rocket,
     name: "Easy to Sign Up",
     desc: "Link your debit card, verify your identity and be sending in minutes.",
   },
   {
-    icon: "IconSupport",
+    icon: support,
     name: "24/7 Support",
     desc: "We are available to chat 24/7 if you need any help.",
   },
   {
-    icon: <PiRocketLaunchDuotone />,
+    icon: rocket,
     name: "Fast & Convenient",
     desc: "Skip the line and send money from your phone to your loved ones in seconds.",
   },
   {
-    icon: <PiShieldCheckDuotone />,
+    icon: sheild,
     name: "Trusted and Legitimate",
     desc: "Trusted by over 1 million users, BNB Cash App is operated by BNB Transfer.",
   },
@@ -32,21 +36,21 @@ const FeaturesSection = () => {
     <section className="features">
       <div className="features_container">
         <div className="features_container_content">
-          <div className="features_container_content_card">
-            {features.map((feature, index) => (
-              <div className="features_container_content_card" key={index}>
-                <div className="features_container_content_card_icon">
-                  {feature.icon}
-                </div>
-                <div className="features_container_content_card_title">
-                  {feature.name}
-                </div>
-                <div className="features_container_content_card_desc">
-                  {feature.desc}
-                </div>
+          {/* <div className="features_container_content_card"> */}
+          {features.map((feature, index) => (
+            <div className="features_container_content_card" key={index}>
+              <div className="features_container_content_card_icon">
+                <Image src={feature.icon} alt="icon" />
               </div>
-            ))}
-          </div>
+              <div className="features_container_content_card_title">
+                {feature.name}
+              </div>
+              <div className="features_container_content_card_desc">
+                {feature.desc}
+              </div>
+            </div>
+          ))}
+          {/* </div> */}
         </div>
       </div>
     </section>

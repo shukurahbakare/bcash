1142cc52bfbf4b0268bd60bfc7e43c209886ac85
drdonice <doniceubaru1@gmail.com>
2024-07-08 10:43:58 +0100

    Fixed UI

--- src/components/modules/home/hero/exchange/index.tsx
+++ src/components/modules/home/hero/exchange/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import "./style.scss";
+import { CountrySelect } from "@/components/common/dropdown";
 
 const Exchange = () => {
   return (
@@ -12,14 +15,21 @@ const Exchange = () => {
         <div className="exchange-section_details_content">
           <h2>Calculate Rates</h2>
           <div className="exchange_cards">
-            <div>
-              <span>You Send</span>
-              <p>$0</p>
+            <div className="exchange_cards_card">
+              <div>
+                <span className="label">You Send</span>
+                <p>$0</p>
+              </div>
+              <CountrySelect />
             </div>
-            <div>
-              <span>You Send</span>
-              <p>₦0</p>
+            <div className="exchange_cards_card">
+              <div>
+                <span className="label">Receiver Gets</span>
+                <p>$0</p>
+              </div>
+              <CountrySelect />
             </div>
+           
             <p className="exchange-section_footer">
               Exchange Rate: 1 GBP = 158.48 Ksh <br />
               Transfer fees: 0.00 GBP

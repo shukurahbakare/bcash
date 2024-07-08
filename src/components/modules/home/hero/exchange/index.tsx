784fe6aa5560262c2905c219e62c8b8fdb2f3f5f
drdonice <doniceubaru1@gmail.com>
2024-07-08 10:51:12 +0100

    FIxed

--- src/components/modules/home/hero/exchange/index.tsx
+++ src/components/modules/home/hero/exchange/index.tsx
@@ -18,17 +18,18 @@ const Exchange = () => {
             <div className="exchange_cards_card">
               <div>
                 <span className="label">You Send</span>
-                <p>$0</p>
+                <label>$ <input type="number" /></label>
               </div>
               <CountrySelect />
             </div>
             <div className="exchange_cards_card">
               <div>
                 <span className="label">Receiver Gets</span>
-                <p>$0</p>
+                <label>$ <input type="number" /></label>
               </div>
               <CountrySelect />
             </div>
+           
            
             <p className="exchange-section_footer">
               Exchange Rate: 1 GBP = 158.48 Ksh <br />
